394bf4ff3ee89ec28e234340139190c4d9fa5445
brandonbrown <brandon@bybrandonbrown.com>
2019-12-09 11:31:12 -0500

    move old main to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import hello from '../assets/indexImages/hello.gif'
 
 // Import typefaces
 
@@ -8,8 +9,32 @@ class Footer extends React.Component {
     const thisyear = dt.getFullYear();
     return (
       <footer>
-        <p>Copyright {thisyear} <a href="about" title="About Brandon Brown">Brandon Brown</a></p>
-        <p><a href="mailto:info@bybrandonbrown.com" title="Contact Brandon through email">Contact Me</a></p>
+        <div className="content-wrapper">
+
+          <section className="personal-info">
+            <img className="helloImg" src={hello} alt="Hello!" />
+            <h1 className="introduction">
+              <span>I'm Brandon, a Design &amp; Web Development Educator highly interested in</span>
+              <span>using technology to make the health & fitness industries stronger </span>
+              <span>through meaningful applications and personalized solutions.</span>
+            </h1>
+            <h6>You will also find me on these networks:</h6>
+            <ul className="sociallinks">
+              <li>twitter <span>&mdash;</span> <a href="https://twitter.com/bybrandonbrown" title="Follow Brandon on twitter">@bybrandonbrown</a></li>
+              <li>dribbble <span>&mdash;</span> <a href="https://dribbble.com/brandonbrown" title="Follow Brandon on dribbble">@brandonbrown</a></li>
+              <li>medium <span>&mdash;</span> <a href="https://medium.com/@bybrandonbrown" title="Folow Brandon on Medium">@bybrandonbrown</a></li>
+              <li>github <span>&mdash;</span> <a href="http://github.com/brandonbrown" title="Follow Brandon on GitHub">@brandonbrown</a></li>
+              <li>instagram <span>&mdash;</span> <a href="https://instagram.com/heyinstructor" title="Follow Brandon on instagram">@heyinstructor</a></li>
+              <li>youtube <span>&mdash;</span> <a href="#">@PointLinePlaneXYZ</a></li>
+              <li>email <span>&mdash;</span> <a href="mailto:hey@bybrandonbrown.com">hey@bybrandonbrown.com</a></li>
+            </ul>
+          </section>
+          <section className="footerDetails">
+            <p>Copyright {thisyear} <a href="about" title="About Brandon Brown">Brandon Brown</a></p>
+            <p><a href="mailto:info@bybrandonbrown.com" title="Contact Brandon through email">Contact</a></p>
+          </section>
+        </div>
+
       </footer>
     )
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import { rhythm, scale } from '../utils/typography'
 
 import '../sass/s.scss'
 
-import hello from '../assets/indexImages/hello.gif'
 import profile from '../assets/indexImages/profile.jpg'
 import calendar from '../assets/indexImages/31.svg'
 import mmf from '../assets/indexImages/mmf-logo-white-mark.svg'
@@ -115,27 +114,7 @@ export default function Writing({ data }) {
         />
 
       </section>
-      <div className="content-wrapper">
 
-        <section className="personal-info">
-          <img className="helloImg" src={hello} alt="Hello!" />
-          <h1 className="introduction">
-            <span>I'm Brandon, a Design &amp; Web Development Educator highly interested in</span>
-            <span>using technology to make the health & fitness industries stronger </span>
-            <span>through meaningful applications and personalized solutions.</span>
-          </h1>
-          <h6>You will also find me on these networks:</h6>
-          <ul className="sociallinks">
-            <li>twitter <span>&mdash;</span> <a href="https://twitter.com/bybrandonbrown" title="Follow Brandon on twitter">@bybrandonbrown</a></li>
-            <li>dribbble <span>&mdash;</span> <a href="https://dribbble.com/brandonbrown" title="Follow Brandon on dribbble">@brandonbrown</a></li>
-            <li>medium <span>&mdash;</span> <a href="https://medium.com/@bybrandonbrown" title="Folow Brandon on Medium">@bybrandonbrown</a></li>
-            <li>github <span>&mdash;</span> <a href="http://github.com/brandonbrown" title="Follow Brandon on GitHub">@brandonbrown</a></li>
-            <li>instagram <span>&mdash;</span> <a href="https://instagram.com/heyinstructor" title="Follow Brandon on instagram">@heyinstructor</a></li>
-            <li>youtube <span>&mdash;</span> <a href="#">@PointLinePlaneXYZ</a></li>
-            <li>email <span>&mdash;</span> <a href="mailto:hey@bybrandonbrown.com">hey@bybrandonbrown.com</a></li>
-          </ul>
-        </section>
-      </div>
       </div>
     )
 }
