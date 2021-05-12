229a34965dcd65ba18c950f8be87356354e6e48b
brandonbrown <brandon@centraflux.com>
2021-05-11 23:04:30 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b6e6e919d795d32f2909.js"
+    "url": "webpack-runtime-4ffd9ef62d76003dc03b.js"
   },
   {
     "url": "framework-2eaa3c90842439cc8720.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "97853ac4bcfeeaa8dd674e4d1dd4495f"
+    "revision": "4659d9b6eda75c839442b1cb913f3256"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-859e22a7cba0ed2e01d8.js"
